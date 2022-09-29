886b3de955cd6434e47f9605acef018b0ebbb98b
seprator <seprator.hd@gmail.com>
2022-09-29 13:07:33 +0300

    step 24

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,10 +1,10 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:notes_app/constants/routes.dart';
 import 'package:notes_app/services/auth/auth_exeptions.dart';
 import 'package:notes_app/services/auth/bloc/auth_event.dart';
+import 'package:notes_app/services/auth/bloc/auth_state.dart';
 import 'package:notes_app/utilities/dialogs/error_dialog.dart';
 
 import '../services/auth/bloc/auth_bloc.dart';
@@ -57,44 +57,40 @@ class _LoginViewState extends State<LoginView> {
             controller: _password,
             decoration: const InputDecoration(hintText: "Enter Your password"),
           ),
-          TextButton(
-            onPressed: () async {
-              final email = _email.text;
-              final password = _password.text;
-              try {
+          BlocListener<AuthBloc, AuthState>(
+            listener: (context, state) async {
+              if (state is AuthStateLoggedOut) {
+                if (state.exception is UserNotFoundAuthException) {
+                  await showErrorDialog(context, 'User not found');
+                } else if (state.exception is WrongPasswordAuthException) {
+                  await showErrorDialog(context, 'Wrong credentials');
+                } else if (state.exception is GenericAuthException) {
+                  await showErrorDialog(context, 'authentication error');
+                }
+              }
+            },
+            child: TextButton(
+              onPressed: () async {
+                final email = _email.text;
+                final password = _password.text;
                 context.read<AuthBloc>().add(
                       AuthEventLogIn(
                         email,
                         password,
                       ),
                     );
-              } on UserNotFoundAuthException {
-                await showErrorDialog(
-                    context,
-                    'User not found',
-                  );
-              } on WrongPasswordAuthException {
-                await showErrorDialog(
-                    context,
-                    'Wrong password',
-                  );
-              } on GenericAuthException {
-                await showErrorDialog(
-                    context,
-                  'authentication error',
-                );
-              }
-            },
-            child: const Text("Login"),
+                
+              },
+              child: const Text("Login"),
+            ),
           ),
           TextButton(
-              onPressed: (() {
-                Navigator.of(context)
-                    .pushNamedAndRemoveUntil(
+            onPressed: (() {
+              Navigator.of(context).pushNamedAndRemoveUntil(
                 registerRoute,
                 (route) => false,
               );
-              }),
+            }),
             child: const Text("Not Registered Yet? Register Here!"),
           )
         ],
@@ -102,5 +98,3 @@ class _LoginViewState extends State<LoginView> {
     );
   }
 }
-
-
